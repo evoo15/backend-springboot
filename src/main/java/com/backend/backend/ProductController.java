748fd85dd412584b540726635e389872fe0fec96
evoo15 <superyassine15@gmail.com>
2018-10-05 21:15:38 +0100

    CORS

--- src/main/java/com/backend/backend/ProductController.java
+++ src/main/java/com/backend/backend/ProductController.java
@@ -6,10 +6,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
